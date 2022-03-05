b24eb86435256bd63a51083792342ea66c55e93a
Hunter Bourgeois <bahunter@gmail.com>
2022-03-05 15:55:13 +0100

    learning hooks

--- Adopt-me/src/App.js
+++ Adopt-me/src/App.js
@@ -1,13 +1,12 @@
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1> Adopt Me </h1>
-      <Pet name="Jamon" animal="dog" breed="dacshund" />
-      <Pet name="Doufu" animal="dog" breed="Spanish Water Dog" />
-      <Pet name="Gretta" animal="dog" breed="Giant Shnowzer" />
+      <SearchParams />
     </div>
   );
 };
